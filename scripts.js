66e7d18adc3c0b942107947632b4cc988ea273e0
1applefridge <lucasccng@gmail.com>
2025-08-01 19:47:43 -0400

    Changed the footer and header to be inserted dynamically using JavaScript

--- scripts.js
+++ scripts.js
@@ -16,4 +16,19 @@ document.addEventListener("DOMContentLoaded", function () {
       });
     })
     .catch(error => console.error('Error loading blog posts:', error));
+    // Load Header
+  fetch('header.html')
+    .then(response => response.text())
+    .then(data => {
+      document.getElementById('header').innerHTML = data;
+    })
+    .catch(error => console.error('Could not load header:', error));
+
+  // Load Footer
+  fetch('footer.html')
+    .then(response => response.text())
+    .then(data => {
+      document.getElementById('footer').innerHTML = data;
+    })
+    .catch(error => console.error('Could not load footer:', error));
 });
