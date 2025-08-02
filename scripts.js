a7ed69b307e61b856ea1416cd23ec1f4e513ca09
1applefridge <lucasccng@gmail.com>
2025-08-02 14:31:10 -0400

    Fixed active page only being "about"

--- scripts.js
+++ scripts.js
@@ -22,6 +22,18 @@ document.addEventListener("DOMContentLoaded", function () {
     .then(response => response.text())
     .then(data => {
       document.getElementById('header').innerHTML = data;
+
+      const currentPage = window.location.pathname.split('/').pop(); 
+      const navLinks = document.querySelectorAll('.nav-link');
+      navLinks.forEach(link => {
+      const href = link.getAttribute('href');
+      if (href === currentPage) {
+        link.classList.add('active');
+      } else {
+        link.classList.remove('active');
+      }
+      
+    });
       darkMode();
     })
     .catch(error => console.error('Could not load header:', error));
